7c0b41e44ee41f3857fa6e85aa0d7fec77f0caee
Luojin <2661468606@qq.com>
2021-03-07 23:05:43 +0800

    热销商品和品牌销量统计

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,25 @@
+<!--
+针对于 /rankpage 这条路径而显示出来的
+在这个组件中, 通过子组件注册的方式, 要显示出Rank.vue这个组件
+-->
+<template>
+  <div class="com-page">
+    <Product></Product>
+  </div>
+</template>
+
+<script>
+import Product from '@/components/Product'
+export default {
+  data () {
+    return {}
+  },
+  methods: {},
+  components: {
+    Product: Product
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = { 
+    devServer: { 
+        port: 8889, // 端口号的配置 
+        open: true // 自动打开浏览器 
+    } 
+}
